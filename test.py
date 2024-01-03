b26d820836ffafd54dd6b33f675a96e0a45d41af
Aryan Saharan <124769240+AryanSaharan01@users.noreply.github.com>
2024-01-03 23:32:51 +0530

    commit

--- test.py
+++ test.py
@@ -3,32 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/crazyblog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
 db = SQLAlchemy(app)
 
-class Contact(db.Model):
+
+class Contacts(db.Model):
     '''
-        sno, name phone_no, msg, date, email
-        '''
+    sno, name phone_num, msg, date, email
+    '''
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(20), nullable=False)
-    phone_no = db.Column(db.String(12), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
     msg = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable=True)
-
+    email = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
-def index():   #the def fuction name should be different in each new app
+def home():
     return render_template('index.html')
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/post")
-def pricing():
-    return render_template('post.html')
 
 @app.route("/contact", methods = ['GET', 'POST'])
 def contact():
@@ -38,12 +36,12 @@ def contact():
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
-        entry = Contact(name=name, phone_no = phone, msg = message, date= datetime.now(),email = email )
+        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
         db.session.add(entry)
         db.session.commit()
     return render_template('contact.html')
 
 
-
-# it will update any changes
 app.run(debug=True)
+
+
